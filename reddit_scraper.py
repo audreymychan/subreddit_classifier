052181107f211ca10de859e6de837608ade2c323
audreymychan <audreymychan@gmail.com>
2019-06-20 14:51:16 -0400

    Refactor code

--- reddit_scraper.py
+++ reddit_scraper.py
@@ -1,18 +1,66 @@
-import datetime
+# This script accesses Reddit's API to collect text of posts from
+# 'nopoo' and 'constipation' subreddits
+
 from time import sleep
 import pandas as pd
 import requests
 
-# function to return json of each page
+
+def fetch_subreddit(subreddit, pages=2):
+    """Function to return text from all posts from a specified
+    number of pages (default=150) for a subreddit.
+
+    Parameters
+    ----------
+    subreddit : str
+        Subreddit name (i.e. 'nopoo').
+    pages : int
+        Number of pages to scrape from a subreddit.
+
+    Returns
+    -------
+    all_posts : list
+        Description of returned object.
+        For example,
+            [
+            {'subreddit': 'NoPoo',
+            'selftext': 'Community recommended
+            products [here](https://docs.google.com/document/d/1BaAzPnW
+            7gyMDf9XrK0T9oP2cvsf0DmYFb7sQsUulJP8/edit#)',
+            'name': 't3_butuoi'},
+            {'subreddit': 'NoPoo',
+            'selftext': 'I feel like I’m drumming on about this a lot lately,
+             so I’m making a specific post.\n\n**Do not apply BS dry.**\n\n**
+             Do not make a paste.**\n\n**Do not make a 50/50 mix with ACV.**\n
+             \nDoing those things **WILL** damage your hair.',
+             'name': 't3_c2ykn0'},
+             ...]
+    """
+    all_posts = []
+    after = ''
+    for i in range(pages):
+        # try:
+        print(f'Fetching Page {i + 1}')  # progress print
+        page = fetch_page(subreddit, after)
+        parsed_posts, after = parse_page(page)
+        all_posts.extend(parsed_posts)
+        sleep(5)
+        # except:
+        #     print("An exception occurred")
+    return all_posts
+
+
 def fetch_page(subreddit, after=''):
+    """Function to return json of each page."""
     URL = f'https://www.reddit.com/r/{subreddit}.json'
     headers = {'User-Agent': 'My User Agent 1.0'}
     params = {'after': after}
     r = requests.get(URL, headers=headers, params=params)
     return r.json()['data']['children']
 
-# function that calls parse_post() for each page
+
 def parse_page(page):
+    """Function that calls parse_post() for each page."""
     parsed_posts = []
     after = ''
     for post in page:
@@ -21,26 +69,14 @@ def parse_page(page):
         parsed_posts.append(p)
     return parsed_posts, after
 
-# function to get the text attached to the 'subreddit', 'selftext', 'name' tags of each post
+
 def parse_post(post):
+    """Function to get the text attached to the 'subreddit',
+    'selftext', and 'name' tags of each post."""
     keep = ['subreddit', 'selftext', 'name']
     data = post['data']
     return {k: v for k, v in data.items() if k in keep}
 
-# function to return text from all posts from 150 pages for a subreddit, this will take some time
-def fetch_subreddit(subreddit, pages = 150):
-    all_posts = []
-    after = ''
-    for i in range(pages):
-        # try:
-        print(f'Fetching Page {i + 1}')
-        page = fetch_page(subreddit, after)
-        parsed_posts, after = parse_page(page)
-        all_posts.extend(parsed_posts)
-        sleep(5)
-        # except:
-        #     print("An exception occurred")
-    return all_posts
 
 if __name__ == "__main__":
 
@@ -53,7 +89,7 @@ if __name__ == "__main__":
     # converting lists to pandas dataframes, and concatenating them together
     nopoo = pd.DataFrame(nopoo_posts)
     constipation = pd.DataFrame(constipation_posts)
-    df = pd.concat([constipation, nopoo]).reset_index(drop = True)
+    df = pd.concat([constipation, nopoo]).reset_index(drop=True)
 
     # saving results to csv (in case something goes wrong)
     df.to_csv('data.csv')
